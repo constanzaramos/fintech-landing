50837b427702742810319677fc3734c5176ff9ec
Constanza Ramos <c.ramosgamboa@gmail.com>
2025-07-23 20:01:12 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
         <Hero />
         <Benefits />
+        <Footer />
     </>
   );
 }
